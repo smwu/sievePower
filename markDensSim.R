6ecc11e7e5718a9347530618b16dc03a200df246
smwu <swu18@uw.edu>
2018-10-10 14:17:49 -0700

    Removed irrelevant code for tests and confidence intervals. Added function for calculating lambda for T and C

--- markDensSim.R
+++ markDensSim.R
@@ -1,5 +1,4 @@
 source("markHR.R")
-#source("covEst.R")
 
 library(survival)
 
@@ -12,17 +11,28 @@ Ymat <- function(X,u,ssize) drop(X>=matrix(u,ssize,length(u),byrow=TRUE))
 g <- function(theta,V){ exp(drop(V %*% theta)) }
 dG <- function(theta,V){ t(g(theta,V) * V) }
 
+# Calculation of lambdaT and lambdaC:
+# # E.g. for 704:
+# Np = 900
+# np = 34
+# taumax = 80  # weeks
+calcRates <- function(Np, np, taumax) {
+  lambdaT <- (log(1 - (1 + 0.1*Np/np)*(np/Np)))/(-taumax*(1 + 0.1*Np/np))
+  lambdaC <- 0.1*Np/np*lambdaT
+  return(c(lambdaT, lambdaC))
+}
+
+# n = number of subjects in vaccine group
 simulOne <- function(beta,gamma,n){
-  ssize <- 2*n
-  Z <- rep(0:1, each=n)
-  T0 <- rexp(n,lambda)    
-  T1 <- rexp(n,lambda*exp(gamma))
-  T <- c(T0,T1)
-  C <- runif(ssize, 0, 15)
-  X <- pmin(T,C,3)
-  d <- ifelse(T<=pmin(C,3),1,0)
-  nInf0 <- sum(d*(1-Z))
-  nInf1 <- sum(d*Z)
+  Z <- c(rep(0, n), rep(1, 2*n))   # treatment group
+  T0 <- rexp(n, lambdaT)    # failure time for placebo
+  T1 <- rexp(2*n, lambdaT*exp(gamma))  # failure time for vaccine
+  T <- c(T0,T1)   # failure times
+  C <- rexp(3*n, lambdaC)   # censoring times
+  X <- pmin(T,C,3)   # observed time is minimum of failure, centoring, and study time
+  d <- ifelse(T<=pmin(C,3),1,0)   # failure indicator (0 if censored)
+  nInf0 <- sum(d*(1-Z))  # number of infected in placebo group
+  nInf1 <- sum(d*Z)      # number of infected in vaccine group
   V0 <- log(1-(1-exp(-2))*runif(n))/(-2)
   V1 <- log(1+(beta-2)*(1-exp(-2))*runif(n)/(2*exp(alpha(beta))))/(beta-2)
   V <- c(V0,V1)
@@ -34,36 +44,14 @@ simulOne <- function(beta,gamma,n){
     thetaHat <- dRatio$coef
     vthetaHat <- dRatio$var[1:2,1:2]
     gammaHat <- phReg$coef
-    vgammaHat <- drop(phReg$var) 
-    covThG <- covEst(X,d,V,Z,thetaHat[1:2],thetaHat[3],gammaHat)
-    covThG <- numeric(2)
+    vgammaHat <- drop(phReg$var)
     
     ve <- VE(v,thetaHat[1],thetaHat[2],gammaHat)
-    Sigma <- cbind(rbind(vthetaHat,covThG), c(covThG,vgammaHat))
-    se <- seVE(v,Sigma,thetaHat[1],thetaHat[2],gammaHat)
-    ci <- c(ve + qnorm(0.975)*se %o% c(-1,1))
-    
-    ### Wald test of H0: VE(v)=VE
-    waldH0 <- thetaHat[2]/sqrt(vthetaHat[2,2])
-    
-    ### Wald test of H00: VE(v)=0
-    waldH00 <- drop(t(c(thetaHat[2], gammaHat)) %*% solve(Sigma[2:3,2:3]) %*% c(thetaHat[2], gammaHat))
-    
-    ### one-sided weighted Wald-type test of H00: VE(v)=0 vs alternatives where VE>0 and VE(v) is decreasing
-    weighted.waldH00 <- (thetaHat[2]/vthetaHat[2,2] - gammaHat/vgammaHat)/
-      sqrt(1/vthetaHat[2,2] + 1/vgammaHat - 2*covThG[2]/(vthetaHat[2,2]*vgammaHat))
-    
-    ### log-rank test of the equality of time-to-infection distributions (test statistic and p-value)
-    logrank <- survdiff(Surv(X,d)~Z)$chisq
-    logrank.pval <- 1-pchisq(logrank,1)
     
     ### likelihood ratio test of the null hypothesis that beta=0
     lrBeta.pval <- LRtest(V[d==1],Z[d==1],thetaHat[-length(thetaHat)],thetaHat[length(thetaHat)])$pval
     
-    ### Kolmogorov-Smirnov-type test of conditional independence between T and V given Z
-    # indTV.pval <- bootpval(cbind(X,V,d))
-    
-    return( c(beta, gamma, nInf0, nInf1, ve, se, ci, waldH0, waldH00, weighted.waldH00, logrank.pval, lrBeta.pval, thetaHat[-length(thetaHat)], gammaHat) )
+    return( c(beta, gamma, nInf0, nInf1, ve, lrBeta.pval, thetaHat[-length(thetaHat)], gammaHat) )
     
   }
 }
