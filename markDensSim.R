8d6eb2fbe3adc6219865a5de07b3cba5aed75620
smwu <swu18@uw.edu>
2018-10-25 16:49:04 -0700

    Added functions for simulating mark variable using nonparametric density estimation

--- markDensSim.R
+++ markDensSim.R
@@ -1,116 +1,161 @@
 source("markHR.R")
 library(survival)
 
-# Calculation of lambdaT and lambdaC
-calcRates <- function(Np, np, taumax) {
-  lambdaT <- (log(1 - (1 + 0.1*Np/np)*(np/Np)))/(-taumax*(1 + 0.1*Np/np))
-  lambdaC <- 0.1*Np/np*lambdaT
-  return(c(lambdaT, lambdaC))
-}
 
-# Calculation of true alpha, beta, and gamma parameters
-calcTrueParams <- function (lambdaVraw, markVE) {
-  beta <- -log(1-markVE)/45.5
-  alpha <- log(1 - beta/lambdaVraw)  # alpha = log(1 + (log(1-markVE)/(45.5*lambdaVraw)))
-  phi <- (log(1 - markVE) - 50.5*beta)/2  # phi = 48/45.5*log(1-markVE)
-  gamma <- phi - alpha  # gamma = log(1-markVE)*48/45.5 - log(1 + log(1-markVE)/(45.5*lambdaVraw))
-  return(c(beta, alpha, gamma))
-}
-
-# calculation of VE
+# 've' returns vaccine efficacy values given parameters 'alpha', 'beta', and 'gamma' 
 ve <- function(v, a, b, g){ 1-exp(a+b*v+g) }
 
-# Simulate data and run likelihood ratio test
-# n = number of subjects in placebo group = Np
-simulOne <- function(Np, np, lambdaVraw, markVE, taumax){
+# 'dPredict' returns nonparametric density estimates at 'x'
+# 'npdensityObject' is the output object from 'npudens'
+# 'varName' is the variable name used in the formula in 'npudensbw'
+dPredict <- function(x, npdensityObject, varName){
+  newData <- data.frame(x)
+  colnames(newData) <- varName
+  return(predict(npdensityObject, newdata=newData))
+}
+
+# 'f0' is the density of the mark variable in the placebo group
+# density is given for values of `v`
+f0 <- function(v, dens, varName){
+  dPredict(v, dens, varName)
+}
+
+# 'f1' is the density of the mark variable in the vaccine group
+# density if given for values of `v`
+f1 <- function(v, dens, varName, alpha, beta){
+  f0(v, dens, varName)*exp(alpha + beta*v)
+}
+
+# 'intf1' is the equation that is solved to obtain parameter 'alpha'
+intf1 <- function(alpha, dens, varName, beta){
+  integrate(f1, lower=-100, upper=100, dens=dens, varName="log10ic50", alpha=alpha, beta=beta, subdivisions=2000)$value - 1
+}
+
+
+# `simulOne` simulates data and runs 1-sided Wald tests for H0: VE(v)=0 and H0: VE(v)=VE and a likelihood ratio test for H0: VE(v)=VE 
+# The function returns a list containing the simulated mark variable, the p-values of the hypothesis tests, 
+# and the parameters estimated in the mark-density ratio and the marginal hazard function 
+  # 'Np' is number of subjects in placebo group
+  # 'np' number of cases in placebo group (fixed at around 34)
+  # 'markVE' is the upper vaccine efficacy cutoff, defined as the vaccine efficacy achieved when the mark variable equals 0.3
+  # 'taumax' is the follow-up time (in weeks)
+  # 'dens' is the output object from the 'npudens' function in the 'np' package
+  # 'varName' is the variable name used in the formula in 'npudensbw'
+simulOne <- function(Np, np, markVE, taumax, dens, varName){
   
+  # rate parameters for failure time T and censoring time C
   lambdaT <- (log(1 - (1 + 0.1*Np/np)*(np/Np)))/(-taumax*(1 + 0.1*Np/np))
   lambdaC <- 0.1*Np/np*lambdaT
   
-  beta <- -log(1-markVE)/45.5
-  alpha <- log(1 - beta/lambdaVraw)  # alpha = log(1 + (log(1-markVE)/(45.5*lambdaVraw)))
-  phi <- (log(1 - markVE) - 50.5*beta)/2  # phi = 48/45.5*log(1-markVE)
+  # true parameters obtained by solving the system of equations specifying cutoff VE values
+  beta <- log(1-markVE)/log10(0.3/50)
+  alpha <- uniroot(intf1, interval=c(-10,10), dens=dens, varName=varName, beta=beta)$root  
+  phi <- (log(1 - markVE) - log10(0.3*50)*beta)/2  
   gamma <- phi - alpha
+ 
   
-  Z <- c(rep(0, Np), rep(1, 2*Np))   # treatment group
-  T0 <- rexp(Np, lambdaT)    # failure time for placebo
-  T1 <- rexp(2*Np, lambdaT*exp(gamma))  # failure time for vaccine
-  T <- c(T0,T1)   # failure times
-  C <- rexp(3*Np, lambdaC)   # censoring times
-  X <- pmin(T,C, taumax)   # observed time is minimum of failure, centoring, and study time
-  d <- ifelse(T <= pmin(C,taumax),1,0)   # failure indicator (0 if censored)
-  nInf0 <- sum(d*(1-Z))  # number of infected in placebo group
-  nInf1 <- sum(d*Z)      # number of infected in vaccine group
-  Vraw0 <- rexp(Np, lambdaVraw)  # raw mark in placebos
-  Vraw1 <- rexp(2*Np, (lambdaVraw - beta))  # raw mark in vaccinees
-  Vraw <- c(Vraw0,Vraw1)
-  V <- ifelse(Vraw < 0.00076, log10(0.00076), ifelse(Vraw <= 50, log10(Vraw), log10(50)))  # scaled mark
-  dRatio <- densRatio(V[d==1],Z[d==1])  # mark is only observed for cases
+  Z <- c(rep(0, Np), rep(1, 2*Np))        # treatment group
+  T0 <- rexp(Np, lambdaT)                 # failure time for placebo
+  T1 <- rexp(2*Np, lambdaT*exp(gamma))    # failure time for vaccine
+  T <- c(T0,T1)                           # failure times
+  C <- rexp(3*Np, lambdaC)                # censoring times
+  X <- pmin(T,C, taumax)                  # observed time: minimum of failure, censoring, and study time
+  d <- ifelse(T <= pmin(C,taumax),1,0)    # failure indicator (0 if censored)
+  nInf0 <- sum(d*(1-Z))                   # number of infected in placebo group
+  nInf1 <- sum(d*Z)                       # number of infected in vaccine group
+  
+  Xpoints <- seq(-3,3,len=25000)                    # fine grid of points ranging from -3 to 3 to be sampled from
+  prob0 <- f0(Xpoints, dens, varName)               # sampling probability for placebos, using nonparametric density estimates
+  prob1 <- f1(Xpoints, dens, varName, alpha, beta)  # sampling probabiliy for vaccinees, using nonparametric density estimates 
+  V0 <- sample(Xpoints, size=Np, prob=prob0)        # sample with replacement with probability prob0 to simulate mark in placebos 
+  V1 <- sample(Xpoints, size=2*Np, prob=prob1)      # sample with replacement with probability prob1 to simulate mark in vaccinees
+  V <- c(V0, V1)                                    # mark variable
+  V <- ifelse(V < log10(0.00076), log10(0.00076), ifelse(V <= log10(50), V, log10(50)))  # mark variable with extreme values censored
+    
+    # Vraw0 <- rexp(Np, lambdaVraw)  # raw mark in placebos
+    # Vraw1 <- rexp(2*Np, (lambdaVraw - beta))  # raw mark in vaccinees
+    # Vraw <- c(Vraw0,Vraw1)
+  
+  # calculate mark density ratio
+  # mark is only observed for cases
+  dRatio <- densRatio(V[d==1],Z[d==1])  
   
   if (dRatio$conv) {
     
-    phReg <- coxph(Surv(X,d) ~ Z)
-    thetaHat <- dRatio$coef
-    gammaHat <- phReg$coef
+    phReg <- coxph(Surv(X,d) ~ Z)  # calculate marginal hazard ratio
+    thetaHat <- dRatio$coef        # estimates for betaHat and alphaHat
+    gammaHat <- phReg$coef        
     
-    VE <- ve(V,thetaHat[1],thetaHat[2],gammaHat)
-    VEraw <- ve(Vraw, thetaHat[1], thetaHat[2], gammaHat)
+    VE <- ve(V,thetaHat[1],thetaHat[2],gammaHat)  # vaccine efficacies calculated using estimated parameters
     
-    ### likelihood ratio test of the null hypothesis that beta=0
+    # 2-sided likelihood ratio test of the null hypothesis that beta=0 (constant VE curve)
     lrBeta.pval <- LRtest(V[d==1],Z[d==1],thetaHat[-length(thetaHat)],thetaHat[length(thetaHat)])$pval
     
-    return( list(beta = beta, alpha = alpha, gamma = gamma, lambdaT = lambdaT, lambdaC = lambdaC, 
-                 nInf0 = nInf0, nInf1 = nInf1, V = V, Vraw = Vraw, VE = VE, VEraw = VEraw, mark = V[d==1], trt.id = Z[d==1],
-                 lrBeta.pval = lrBeta.pval, thetaHat = thetaHat[-length(thetaHat)], gammaHat = gammaHat))
-    
-    # return( list(beta = beta, gamma = gamma, nInf0 = nInf0, nInf1 = nInf1, V = V[d==1], Vraw = Vraw[d==1], 
-    #              distGridRaw = distGridRaw, distGrid = distGrid, VEraw = VEraw, VE = VE, Z = Z[d==1], 
-    #              lrBeta.pval = lrBeta.pval, thetaHat = thetaHat[-length(thetaHat)], gammaHat = gammaHat) )
+    return( list(beta = beta, alpha = alpha, gamma = gamma, nInf0 = nInf0, nInf1 = nInf1, V = V, VE = VE, 
+                 mark = V[d==1], trt.id = Z[d==1], lrBeta.pval = lrBeta.pval, 
+                 thetaHat = thetaHat[-length(thetaHat)], gammaHat = gammaHat))
   }
 }
 
 
-# 'plotDistHazVE' generates a plot with point and interval estimates of VE(v), and scatter- and box-plots of univariate index values by treatment in the top panel
-# p-values of tests evaluating H00 and H0 are reported
-# the number of distances is reported
-# 'nSites' is the number of AA sites in the vaccine insert sequence included in calculating the distance
-# distData is a dataframe with columns tx (vaccine=1, placebo=0) and mark (positive, NA values omitted)
-# 'out' is the output from distHazVE
-# 'raw' specifies if the mark varialbe is on the raw scale (TRUE) or not (FALSE)
-simulPlotDistHazVE <- function(result, nSites=NULL, xlab, ylab, title, raw){
-  
-  cexAxis <- 0.75
-  cexLab <- 0.8
-  cexTitle <- 1
-  cexText <- 0.85
-  cexLegend <- 0.8
-  
-  # par(mar=c(3,3,3,2.5), oma=c(0,0,0,0), cex.axis=cexAxis, cex.lab=cexLab, cex.main=cexTitle)  
-  par(cex.axis=cexAxis, cex.lab=cexLab, cex.main=cexTitle)
-  if (raw == TRUE) {
-    plot(result$Vraw, result$VEraw, xlim=range(result$Vraw), ylim=c(0,1), type = "n", xlab="", ylab="", bty="l", main=title)  ###yLim changed
-    mtext(xlab, side=1, line=2, cex=cexLab)
-    mtext(ylab, side=2, line=2.15, las=3, cex=cexLab)
-    abline(h=0, col="gray30", lty="dotted", lwd=2)
-    points(result$Vraw, result$VEraw)
-    ### continuous mark-specific treatment efficacy estimates
-    text(0.2, 0.2, paste0("No. of Cases (V:P): ",sum(result$trt.id==1),":",sum(result$trt.id==0), "\nLR test of H0: \nPE(v) = PE: ",
-                ifelse(result$lrBeta.pval<0.001,"p < 0.001", paste0("p = ",format(result$lrBeta.pval,digits=2)))), pos=4, cex=cexText)
-    
-  } else {
-    plot(result$V, result$VE, xlim=range(result$V), ylim=c(0,1), type = "n", xaxt = "n", xlab="", ylab="", bty="l", main=title)  ###yLim changed
-    grid <- seq(min(result$V), max(result$V), length = 6)
-    axis(side=1, at=grid, labels=round(10^grid,3), las=1)
-    mtext(xlab, side=1, line=2, cex=cexLab)
-    mtext(ylab, side=2, line=2.15, las=3, cex=cexLab)
-    abline(h=0, col="gray30", lty="dotted", lwd=2)
-    points(result$V, result$VE)
-    ### continuous mark-specific treatment efficacy estimates
-    text(x=grid[2], 0.2, paste0("No. of Cases (V:P): ",sum(result$tx==1),":",sum(result$tx==0), "\nLR test of H0: \nPE(v) = PE: ",
-                ifelse(result$lrBeta.pval<0.001,"p < 0.001", paste0("p = ",format(result$lrBeta.pval,digits=2)))), cex=cexText)
-    
-  }
-  
-  if (!is.null(nSites)){mtext(paste0("Number of vaccine insert AA sites = ",nSites), side=1, line=-1, cex=0.8, adj=1)}
-}
 
+# # 'plotDistHazVE' generates a plot with point and interval estimates of VE(v), and scatter- and box-plots of univariate index values by treatment in the top panel
+# # p-values of tests evaluating H00 and H0 are reported
+# # the number of distances is reported
+# # 'nSites' is the number of AA sites in the vaccine insert sequence included in calculating the distance
+# # distData is a dataframe with columns tx (vaccine=1, placebo=0) and mark (positive, NA values omitted)
+# # 'out' is the output from distHazVE
+# # 'raw' specifies if the mark varialbe is on the raw scale (TRUE) or not (FALSE)
+# simulPlotDistHazVE <- function(result, nSites=NULL, xlab, ylab, title, raw){
+#   
+#   cexAxis <- 0.75
+#   cexLab <- 0.8
+#   cexTitle <- 1
+#   cexText <- 0.85
+#   cexLegend <- 0.8
+#   
+#   # par(mar=c(3,3,3,2.5), oma=c(0,0,0,0), cex.axis=cexAxis, cex.lab=cexLab, cex.main=cexTitle)  
+#   par(cex.axis=cexAxis, cex.lab=cexLab, cex.main=cexTitle)
+#   if (raw == TRUE) {
+#     plot(result$Vraw, result$VEraw, xlim=range(result$Vraw), ylim=c(0,1), type = "n", xlab="", ylab="", bty="l", main=title)  ###yLim changed
+#     mtext(xlab, side=1, line=2, cex=cexLab)
+#     mtext(ylab, side=2, line=2.15, las=3, cex=cexLab)
+#     abline(h=0, col="gray30", lty="dotted", lwd=2)
+#     points(result$Vraw, result$VEraw)
+#     ### continuous mark-specific treatment efficacy estimates
+#     text(0.2, 0.2, paste0("No. of Cases (V:P): ",sum(result$trt.id==1),":",sum(result$trt.id==0), "\nLR test of H0: \nPE(v) = PE: ",
+#                 ifelse(result$lrBeta.pval<0.001,"p < 0.001", paste0("p = ",format(result$lrBeta.pval,digits=2)))), pos=4, cex=cexText)
+#     
+#   } else {
+#     plot(result$V, result$VE, xlim=range(result$V), ylim=c(0,1), type = "n", xaxt = "n", xlab="", ylab="", bty="l", main=title)  ###yLim changed
+#     grid <- seq(min(result$V), max(result$V), length = 6)
+#     axis(side=1, at=grid, labels=round(10^grid,3), las=1)
+#     mtext(xlab, side=1, line=2, cex=cexLab)
+#     mtext(ylab, side=2, line=2.15, las=3, cex=cexLab)
+#     abline(h=0, col="gray30", lty="dotted", lwd=2)
+#     points(result$V, result$VE)
+#     ### continuous mark-specific treatment efficacy estimates
+#     text(x=grid[2], 0.2, paste0("No. of Cases (V:P): ",sum(result$tx==1),":",sum(result$tx==0), "\nLR test of H0: \nPE(v) = PE: ",
+#                 ifelse(result$lrBeta.pval<0.001,"p < 0.001", paste0("p = ",format(result$lrBeta.pval,digits=2)))), cex=cexText)
+#     
+#   }
+#   
+#   if (!is.null(nSites)){mtext(paste0("Number of vaccine insert AA sites = ",nSites), side=1, line=-1, cex=0.8, adj=1)}
+# }
+
+
+# # Calculation of lambdaT and lambdaC
+# calcRates <- function(Np, np, taumax) {
+#   lambdaT <- (log(1 - (1 + 0.1*Np/np)*(np/Np)))/(-taumax*(1 + 0.1*Np/np))
+#   lambdaC <- 0.1*Np/np*lambdaT
+#   return(c(lambdaT, lambdaC))
+# }
+# 
+# # Calculation of true alpha, beta, and gamma parameters
+# calcTrueParams <- function (lambdaVraw, markVE) {
+#   beta <- -log(1-markVE)/49.5
+#   alpha <- log(1 - beta/lambdaVraw)  # alpha = log(1 + (log(1-markVE)/(49.5*lambdaVraw)))
+#   phi <- (log(1 - markVE) - 50.5*beta)/2  # phi = 48/49.5*log(1-markVE)
+#   gamma <- phi - alpha  # gamma = log(1-markVE)*48/49.5 - log(1 + log(1-markVE)/(49.5*lambdaVraw))
+#   return(c(beta, alpha, gamma))
+# }
